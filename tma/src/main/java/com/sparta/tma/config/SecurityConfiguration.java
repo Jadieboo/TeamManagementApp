edc0f74bf6c053da244053f7c536ce0e54c70f5d
Jade Sale <jadesale1991@googlemail.com>
2023-07-28 19:26:27 +0100

    set up security filter chain config

--- tma/src/main/java/com/sparta/tma/config/SecurityConfiguration.java
+++ tma/src/main/java/com/sparta/tma/config/SecurityConfiguration.java
@@ -1,4 +1,57 @@
 package com.sparta.tma.config;
 
+import com.sparta.tma.services.AppUserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
+
+    private final AppUserService appUserService;
+
+    public SecurityConfiguration(AppUserService appUserService) {
+        this.appUserService = appUserService;
+    }
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated())
+                .userDetailsService(appUserService);
+
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.httpBasic(Customizer.withDefaults());
+        http.formLogin(Customizer.withDefaults());
+        http.csrf((AbstractHttpConfigurer::disable));
+        return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // might not need this
+    // because of .userDetailsService(appUserService); in the security filter chain
+//    @Bean
+//    public AuthenticationManager authManager(UserDetailsService detailsService) {
+//        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+//        provider.setPasswordEncoder(passwordEncoder());
+//        provider.setUserDetailsService(detailsService);
+//        return new ProviderManager(provider);
+//    }
+
 }
