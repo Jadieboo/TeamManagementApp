88ec0dbccb5bfbdb830ed43e5c12a21e0706b3f3
Jade Sale <jadesale1991@googlemail.com>
2023-08-18 19:56:04 +0100

    added unit tests for setting and updating project and department methods

--- tma/src/test/java/com/sparta/tma/AdminControllerTests.java
+++ tma/src/test/java/com/sparta/tma/AdminControllerTests.java
@@ -9,9 +9,11 @@ import com.sparta.tma.entities.Employee;
 import com.sparta.tma.repositories.DepartmentRepository;
 import com.sparta.tma.repositories.EmployeeRepository;
 import com.sparta.tma.repositories.ProjectRepository;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,19 +48,23 @@ public class AdminControllerTests {
     }
 
     @Test
+    @DisplayName("Create a list of all employee's in the database")
     public void viewAllEmployees() {
+
         List<Employee> employeeList = employeeRepository.findAll();
 
         assertTrue(employeeList.size() > 0);
     }
 
     @Test
-    public void  createNewEmployee() {
-        Employee employee = new EmployeeDAO(employeeRepository, departmentRepository, projectRepository).createNewEmployee(employeeDetails());
+    @DisplayName("Creates a new employee using createNewEmployee() DAO method to set id, first name, last name, role, department and project")
+    public void newEmployeeDaoMethod() {
 
-        String expected = "Employee{id: 0, firstName: Unit, lastName: Test, role: ADMIN, department: Development, project: Web App}";
+        Employee employee = new EmployeeDAO(departmentRepository, projectRepository).createNewEmployee(employeeDetails());
 
         String result = employee.toString();
+
+        String expected = "Employee{id: 0, firstName: Unit, lastName: Test, role: ADMIN, department: Development, project: Web App}";
 
         assertEquals(expected, result);
     }
@@ -66,28 +72,47 @@ public class AdminControllerTests {
     // TODO: add params to unit test or create specific tests, to test null value and values that do not exist for
     //  BOTH department and project
     //  THEN create test branch and commit to github repo
-    @Test
-    public void updateEmployeeDepartment() {
-        Employee employee = new EmployeeDAO(employeeRepository, departmentRepository, projectRepository).createNewEmployee(employeeDetails());
+    @ParameterizedTest
+    @DisplayName("method will handle if the input is uppercase, lowercase, mixed case or has any whitespaces")
+    @CsvSource({
+            "HR, HR",
+            "finance, Finance",
+            "marKetINg, Marketing",
+            "Sales, Sales",
+            "design, Design",
+            " development, Development",
+            " customer Service , Customer Service"
+    })
+    public void setOrUpdateEmployeeDepartment(String department, String expectedDepartment) {
+        Employee employee = new EmployeeDAO(departmentRepository, projectRepository).createNewEmployee(employeeDetails());
 
         logger.info("Employee information before update: {}", employee);
 
-        EmployeeDTO updateDepartment = new EmployeeDTO();
-        updateDepartment.setDepartment("design");
+        EmployeeDTO updatedDepartment = new EmployeeDTO();
+        updatedDepartment.setDepartment(department);
 
-        employee.setDepartment(new DepartmentDAO(departmentRepository).getDepartment(updateDepartment));
+        employee.setDepartment(new DepartmentDAO(departmentRepository).getDepartment(updatedDepartment));
 
         logger.info("Employee information after update: {}", employee);
 
-        String expected = "Design";
         String result = employee.getDepartment().toString();
 
-        assertEquals(expected, result);
+        assertEquals(expectedDepartment, result);
     }
 
-    @Test
-    public void updateEmployeeProject() {
-        Employee employee = new EmployeeDAO(employeeRepository, departmentRepository, projectRepository).createNewEmployee(employeeDetails());
+    @ParameterizedTest
+    @DisplayName("method will handle if the input is uppercase, lowercase, mixed case or has any whitespaces")
+    @CsvSource({
+            "UNASSIGNED, Unassigned",
+            "new starters, New Starters",
+            "Attendance, Attendance",
+            "paYroLl, Payroll",
+            "accOUNTs, Accounts",
+            " Web App Frontend, Web App Frontend",
+            "Logo , Logo"
+    })
+    public void setOrUpdateEmployeeProject() {
+        Employee employee = new EmployeeDAO(departmentRepository, projectRepository).createNewEmployee(employeeDetails());
 
         logger.info("Employee information before update: {}", employee);
 
@@ -102,6 +127,25 @@ public class AdminControllerTests {
         String result = employee.getProject().toString();
 
         assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("Testing getRoleDao method from RoleDAO to set or update role")
+    public void setRoleDaoMethod() {
+
+        Employee employee = new EmployeeDAO(departmentRepository, projectRepository).createNewEmployee(employeeDetails());
+
+        EmployeeDTO setNewRole = new EmployeeDTO();
+        setNewRole.setRole("manager");
+
+        employee.setRole(new RoleDAO().getRole(setNewRole));
+
+        String result = employee.toString();
+
+        String expected = "Employee{id: 0, firstName: Unit, lastName: Test, role: MANAGER, department: Development, project: Web App}";
+
+        assertEquals(expected, result);
+
     }
 
 
--- tma/src/test/java/com/sparta/tma/CreateOrUpdateDaoMethodTests.java
+++ tma/src/test/java/com/sparta/tma/CreateOrUpdateDaoMethodTests.java
@@ -40,81 +40,9 @@ public class CreateOrUpdateDaoMethodTests {
         return employeeDetails;
     }
 
-    @Test
-    @DisplayName("Creating a new employee using createNewEmployee() DAO method to set id, first name, last name, role, department and project")
-    public void newEmployeeDaoMethod() {
 
-        Employee employee = new EmployeeDAO(employeeRepository, departmentRepository, projectRepository).createNewEmployee(employeeDetails());
 
-        String result = employee.toString();
 
-        String expected = "Employee{id: 0, firstName: Unit, lastName: Test, role: ADMIN, department: Development, project: Web App}";
 
-        assertEquals(expected, result.toString());
-    }
 
-    @Test
-    @DisplayName("Testing getRoleDao method from RoleDAO to set or update role")
-    public void setRoleDaoMethod() {
-
-        Employee employee = new EmployeeDAO(employeeRepository, departmentRepository, projectRepository).createNewEmployee(employeeDetails());
-
-        EmployeeDTO setNewRole = new EmployeeDTO();
-        setNewRole.setRole("manager");
-
-        employee.setRole(new RoleDAO().getRole(setNewRole));
-
-        String result = employee.toString();
-
-        String expected = "Employee{id: 0, firstName: Unit, lastName: Test, role: MANAGER, department: Development, project: Web App}";
-
-        assertEquals(expected, result.toString());
-
-    }
-
-    @Test
-    @DisplayName("Testing getDepartmentDao method from DepartmentDAO to set or update department")
-    public void setDepartmentDaoMethod() {
-
-        Employee employee = new EmployeeDAO(employeeRepository, departmentRepository, projectRepository).createNewEmployee(employeeDetails());
-
-        EmployeeDTO setNewDepartment = new EmployeeDTO();
-        setNewDepartment.setDepartment("Sales");
-
-        employee.setDepartment(new DepartmentDAO(departmentRepository).getDepartment(setNewDepartment));
-
-        Employee result = employeeRepository.save(employee);
-
-        String expected = String.format("Employee{id: %s, firstName: %s, lastName: %s, role: %s, department: Sales, project: %s}",
-                employee.getId(),
-                employee.getFirstName(),
-                employee.getLastName(),
-                employee.getRole(),
-                employee.getProject()
-        );
-
-        assertEquals(expected, result.toString());
-    }
-
-    @Test
-    @DisplayName("Testing getProjectDao method from ProjectDAO to set or update project")
-    public void setProjectDaoMethod() {
-        EmployeeDTO empDto = new EmployeeDTO();
-        empDto.setProject("products");
-
-        Employee employee = employeeRepository.findEmployeeById(41);
-        employee.setProject(new ProjectDAO(projectRepository).getProject(empDto));
-
-        Employee result = employeeRepository.save(employee);
-
-        String expected = String.format("Employee{id: %s, firstName: %s, lastName: %s, role: %s, department: %s, project: Products}",
-                employee.getId(),
-                employee.getFirstName(),
-                employee.getLastName(),
-                employee.getRole(),
-                employee.getDepartment()
-        );
-
-        assertEquals(expected, result.toString());
-    }
 }
