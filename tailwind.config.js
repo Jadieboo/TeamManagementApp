6e29653d129ad0e995bd7279393e63a34133328e
Jade Sale <jadesale1991@googlemail.com>
2023-10-04 15:44:25 +0100

    files duplicated and had to restore to a previous commit. Fixed issue and added files to track for tailwindcss installation

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "tma/src/main/resources/templates/*.html"
+  ],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
---
+++ tma/src/main/java/com/sparta/tma/api/controllers/AdminController.java
@@ -0,0 +1,105 @@
+package com.sparta.tma.api.controllers;
+
+import com.sparta.tma.DAOs.*;
+import com.sparta.tma.DTOs.EmployeeDTO;
+import com.sparta.tma.entities.AppUser;
+import com.sparta.tma.entities.Employee;
+import com.sparta.tma.exceptions.EmployeeNotFoundException;
+import com.sparta.tma.repositories.AppUserRepository;
+import com.sparta.tma.repositories.DepartmentRepository;
+import com.sparta.tma.repositories.EmployeeRepository;
+import com.sparta.tma.repositories.ProjectRepository;
+import com.sparta.tma.services.UserAccountService;
+import org.apache.catalina.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+
+@RestController
+public class AdminController {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    @Autowired
+    private AppUserRepository appUserRepository;
+    @Autowired
+    private DepartmentRepository departmentRepository;
+    @Autowired
+    private ProjectRepository projectRepository;
+    @Autowired
+    UserAccountService userAccountService;
+
+
+    @GetMapping("admin/employees")
+    public List<Employee> viewAllEmployees() {
+
+        List<Employee> employeeList = employeeRepository.findAll();
+
+        if (employeeList.size() < 1) {
+            logger.error("Employee list is empty, should be a minimum of one", employeeList);
+        }
+
+        return (!employeeList.isEmpty() ? employeeList : Collections.emptyList());
+    }
+
+    @Transactional
+    @PostMapping("admin/register/employees")
+    public Employee createEmployee(@RequestBody EmployeeDTO employeeDetails) {
+
+        Employee newEmployee = new EmployeeDAO(departmentRepository, projectRepository).createNewEmployee(employeeDetails);
+
+        Employee employee = employeeRepository.saveAndFlush(newEmployee);
+        logger.info("New employee saved, {}", employee);
+
+        logger.info("New user is being created");
+        AppUser newAppUser = userAccountService.createNewAppUser(employeeDetails, employee.getId());
+
+        AppUser appUser = appUserRepository.saveAndFlush(newAppUser);
+        logger.info("New user saved, {}", appUser);
+
+        return employeeRepository.findById(employee.getId()).orElseThrow(() -> new EmployeeNotFoundException("Employee " + employee + " not found/created"));
+    }
+
+    @Transactional
+    @PatchMapping("admin/employees/{id}")
+    public Employee updateEmployee(@PathVariable int id, @RequestBody EmployeeDTO employeeDetails) {
+        Employee updateEmployee = employeeRepository.findEmployeeById(id);
+        logger.info("Changes being made to " + updateEmployee);
+
+        if (!Objects.equals(updateEmployee.getFirstName(), employeeDetails.getFirstName()) || !Objects.equals(updateEmployee.getLastName(), employeeDetails.getLastName())) {
+            updateEmployee.setFirstName(employeeDetails.getFirstName());
+            updateEmployee.setLastName(employeeDetails.getLastName());
+
+            AppUser updateUser = appUserRepository.findByEmployeeId(updateEmployee.getId());
+            updateUser.setUsername(userAccountService.createUserUsername(employeeDetails, updateEmployee.getId()));
+            appUserRepository.saveAndFlush(updateUser);
+        }
+
+        if (!updateEmployee.getRole().toString().equals(employeeDetails.getRole())) {
+            updateEmployee.setRole(new RoleDAO().getRole(employeeDetails));
+
+            AppUser updateUser = appUserRepository.findByEmployeeId(updateEmployee.getId());
+            updateUser.setRole(new RoleDAO().getRole(employeeDetails));
+            appUserRepository.saveAndFlush(updateUser);
+        }
+
+        if (!updateEmployee.getDepartment().toString().equals(employeeDetails.getDepartment())) {
+            updateEmployee.setDepartment(new DepartmentDAO(departmentRepository).getDepartment(employeeDetails));
+            updateEmployee.setProject(projectRepository.findById(1));
+        }
+
+
+        logger.info("Employee information was updated: {}", updateEmployee);
+
+        return employeeRepository.save(updateEmployee);
+    }
+
+}
