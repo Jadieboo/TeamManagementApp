11dc8971b1d23d260791ecb049ba8b838aad75a4
Jade Sale <jadesale1991@googlemail.com>
2023-11-10 16:04:43 +0000

    presenting view employees on front end for employee acces and refactoring: employees can now view colleagues on the frontend and viewing employees/colleagues are now using one single html page for all three role accesses to view employees. Changed login within controllers to implement the change.

--- tma/src/main/java/com/sparta/tma/web/controllers/ViewEmployees.java
+++ tma/src/main/java/com/sparta/tma/web/controllers/ViewEmployees.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -36,22 +37,21 @@ public class ViewEmployees {
     public String getAllEmployeesForAdmin(Model model, Authentication authentication) {
         logger.info("view employees for admin GET method active");
 
-        Optional<AppUser> user = appUserRepository.findByUsername(authentication.getName());
+        AppUser user = appUserRepository.findByUsername(((AppUser) authentication.getPrincipal()).getUsername()).get();
+
+        if (user != null && user.getRole() != null) {
+            getRoleModelAttribute(model, user);
+        }
 
         List<Employee> allEmployeesList = viewEmployeesService.getAllEmployees();
 
         if (!allEmployeesList.isEmpty()) {
-            allEmployeesList.remove(user.get().getEmployee());
+            allEmployeesList.remove(user.getEmployee());
         }
 
-        if (allEmployeesList.size() < 1) {
-            logger.warn("No employees found");
-        } else {
-            logger.info("list size of all employees: {}", allEmployeesList.size());
-            model.addAttribute("employeeList", allEmployeesList);
-        }
+        getPopulatedResultsModelAttribute(model, allEmployeesList);
 
-        return "adminViewAllEmployees";
+        return "viewEmployees";
     }
 
 
@@ -64,6 +64,10 @@ public class ViewEmployees {
 
         AppUser user = appUserRepository.findByUsername(((AppUser) authentication.getPrincipal()).getUsername()).get();
 
+        if (user != null && user.getRole() != null) {
+            getRoleModelAttribute(model, user);
+        }
+
         Department department = user.getEmployee().getDepartment();
 
         List<Employee> employeeList = viewEmployeesService.getEmployeesByDepartment(department);
@@ -72,24 +76,97 @@ public class ViewEmployees {
             employeeList.remove(user.getEmployee());
         }
 
-        if (employeeList.size() < 1) {
-            logger.warn("No employees found");
-        } else {
-            logger.info("list size of all employees: {}", employeeList.size());
-            model.addAttribute("employeeList", employeeList);
-        }
+        getPopulatedResultsModelAttribute(model, employeeList);
 
-        return "managerViewEmployees";
+        return "viewEmployees";
     }
 
 
     /**
      * EMPLOYEE ACCESS
      */
-    @GetMapping("/employee/view/colleagues")
-    public String getEmployeesForEmployee() {
-        List<Employee> list = viewEmployeesService.getEmployeesForEmployee();
 
-        return "welcome";
+    // all colleagues with role employee, no manager or admins
+    @GetMapping("/employee/view/colleagues")
+    public String getColleaguesForEmployee(Model model, Authentication authentication) {
+        logger.info("view colleagues for employee GET method");
+
+        AppUser user = appUserRepository.findByUsername(((AppUser) authentication.getPrincipal()).getUsername()).get();
+
+        if (user != null && user.getRole() != null) {
+            getRoleModelAttribute(model, user);
+        }
+
+        Department department = user.getEmployee().getDepartment();
+
+        List<Employee> employeeList = viewEmployeesService.getEmployeesByDepartment(department);
+
+        if (!employeeList.isEmpty()) {
+            employeeList.remove(user.getEmployee());
+        }
+
+        getPopulatedResultsModelAttribute(model, employeeList);
+
+        return "viewEmployees";
     }
+
+    private void getPopulatedResultsModelAttribute(Model model, List<Employee> employeeList) {
+        if (employeeList.size() < 1) {
+            logger.warn("No employees found");
+            model.addAttribute("results_not_found", true);
+            model.addAttribute("results_populated", false);
+        } else {
+            logger.info("list size of all employees: {}", employeeList.size());
+            model.addAttribute("employeeList", employeeList);
+            model.addAttribute("results_populated", true);
+            model.addAttribute("results_not_found", false);
+        }
+    }
+
+    private void getRoleModelAttribute(Model model, AppUser user) {
+        model.addAttribute("isAdmin", false);
+        model.addAttribute("isManager", false);
+        model.addAttribute("isEmployee", false);
+
+        String role = user.getRole().name().toLowerCase();
+        logger.info("user role: {}", role);
+
+        if (role.equals("admin")) {
+            model.addAttribute("isAdmin", true);
+            logger.info("Setting isAdmin model attribute to true");
+        } else if (role.equals("manager")) {
+            model.addAttribute("isManager", true);
+            logger.info("Setting isManager model attribute to true");
+        } else if (role.equals("employee")) {
+            model.addAttribute("isEmployee", true);
+            logger.info("Setting isEmployee model attribute to true");
+        }
+    }
+
+//    @GetMapping("/colleagues/project")
+//    public String viewAllColleaguesWithinProject(Model model, Authentication authentication) {
+//
+//        logger.info("inside view all colleagues within project GET method");
+//
+//        String username = authentication.getName();
+//
+//        logger.info("authentication: {}, " +
+//                "Username: {}", authentication, username);
+//
+//        Optional<AppUser> user = appUserRepository.findByUsername(username);
+//
+//        List<Employee> employeeList = employeeRepository.findAllByDepartmentAndProjectWithRoleEmployee(user.get().getEmployee().getDepartment(), user.get().getEmployee().getProject());
+//
+//        if (!employeeList.isEmpty()) {
+//            employeeList.remove(user.get().getEmployee());
+//        }
+//
+//        if (employeeList.size() < 1) {
+//            logger.warn("No employees found");
+//        } else {
+//            logger.info("employee list size: {}", employeeList.size());
+//        }
+//
+//        return (!employeeList.isEmpty() ? employeeList : Collections.emptyList());
+//    }
 }
