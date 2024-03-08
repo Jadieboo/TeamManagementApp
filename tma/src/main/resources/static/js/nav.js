b147262735ebf61178b6c83a505296d07d5bc306
Jade Sale <jadesale1991@googlemail.com>
2024-03-08 18:31:36 +0000

    added responsiveness changes to nav for larger screens

--- tma/src/main/resources/static/js/nav.js
+++ tma/src/main/resources/static/js/nav.js
@@ -1,18 +1,16 @@
 const sidebar = document.querySelector('.sidebar');
 sidebar.querySelector('.blocker').onclick = hide;
 
-const open = document.querySelector('.show-nav-btn');
-const close = document.querySelector('.hide-nav-btn');
+const showNavBtn = document.querySelector('.show-nav-btn');
+const hideNavBtn = document.querySelector('.hide-nav-btn');
 
 function show() { // swipe right
+    sidebar.classList.remove('hidden');
     sidebar.classList.remove('sidebar-hidden');
     sidebar.classList.add('sidebar-flex');
-    sidebar.classList.add('lg:flex');
 
-    close.classList.remove('hidden');
-    close.classList.remove('lg:hidden');
-    open.classList.add('hidden');
-    open.classList.remove('lg:hidden');
+    hideNavBtn.classList.remove('hidden');
+    showNavBtn.classList.add('hidden');
 
 
     // document.body.style.overflow = 'hidden';
@@ -22,12 +20,9 @@ function show() { // swipe right
 function hide() { // by blocker click, swipe left, or url change
     sidebar.classList.add('sidebar-hidden');
     sidebar.classList.remove('sidebar-flex');
-    sidebar.classList.remove('lg:flex');
 
-    open.classList.remove('hidden');
-    open.classList.remove('lg:hidden');
-    close.classList.add('hidden');
-    close.classList.add('lg:hidden');
+    showNavBtn.classList.remove('hidden');
+    hideNavBtn.classList.add('hidden');
 
 
 
@@ -35,5 +30,5 @@ function hide() { // by blocker click, swipe left, or url change
     console.log("HIDE");
 }
 function toggle() {
-    sidebar.classList.contains('sidebar-flex') ? hide() : show();
+    sidebar.classList.contains('hidden') || sidebar.classList.contains('sidebar-hidden') ? show() : hide();
 }
