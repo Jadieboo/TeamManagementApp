955fa298dfcfab415092d221b1278980e1a08d11
Jade Sale <jadie@Jades-MacBook-Air.local>
2025-07-24 23:06:15 +0100

    implemented styling for employee and removed placeholder nav option

--- tma/src/main/java/com/sparta/tma/web/LoginController.java
+++ tma/src/main/java/com/sparta/tma/web/LoginController.java
@@ -2,24 +2,18 @@ package com.sparta.tma.web;
 
 import com.sparta.tma.entities.AppUser;
 import com.sparta.tma.entities.Employee;
-import com.sparta.tma.repositories.AppUserRepository;
-import com.sparta.tma.repositories.EmployeeRepository;
 import com.sparta.tma.services.ViewEmployeesService;
-import com.sparta.tma.utils.PopulateEmployeeAttributes;
 import com.sparta.tma.utils.PopulateModelAttributes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 public class LoginController {
@@ -27,9 +21,6 @@ public class LoginController {
     private final ViewEmployeesService viewEmployeesService;
     private final PopulateModelAttributes modelUtil;
 
-//    private final AppUserRepository appUserRepository;
-//    private final EmployeeRepository employeeRepository;
-//    private final PasswordEncoder encoder;
     @Autowired
     public LoginController(ViewEmployeesService viewEmployeesService, PopulateModelAttributes modelUtil) {
         this.viewEmployeesService = viewEmployeesService;
@@ -118,102 +109,5 @@ public class LoginController {
 
         return "employeeHomepage";
     }
-
-    @GetMapping("/welcome")
-    public String welcome(Model model, Authentication authentication) {
-        logger.info("welcome page active");
-
-        Employee employee = ((AppUser) authentication.getPrincipal()).getEmployee();
-        String role = employee.getRole().toString().toLowerCase();
-
-        model.addAttribute("employee", employee);
-        model.addAttribute("role", role);
-
-
-        return "welcome";
-    }
-
-
-
-    // TODO: need to clean up login controller class
-
-
-
-
-
-    // trying to handle LoginSuccessHandler in controller instead of in configuration
-
-//    @Autowired
-//    private AuthenticationManager authenticationManager;
-//
-//    @Autowired
-//    private LoginSuccessHandler loginSuccessHandler;
-//
-//    @PostMapping("/loginHandler")
-//    public String performLogin(HttpServletRequest request, HttpServletResponse response,
-//                               @RequestParam String username, @RequestParam String password) throws ServletException, IOException {
-//
-//        try {
-//            // Create an authentication token with the user's credentials
-//            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
-//
-//            // Perform authentication
-//            Authentication authentication = authenticationManager.authenticate(token);
-//
-//            // If authentication is successful, call the custom success handler
-//            loginSuccessHandler.onAuthenticationSuccess(request, response, authentication);
-//
-//            // Authentication was successful, so no need to proceed further
-//            return null;
-//        } catch (AuthenticationException e) {
-//            // Handle authentication failure (e.g., show an error message)
-//            return "redirect:/login?error";
-//        }
-//    }
-
-
-//    @PostMapping("/loginHandler")
-//    public String loginHandler(@RequestParam String username, @RequestParam String password, Model model) {
-//        logger.info("loginHandler post method is active");
-//
-//        String url = "redirect:/login?error";
-//
-//        Optional<AppUser> user = appUserRepository.findByUsername(username);
-//
-//        if (user.isPresent()) {
-//            logger.info("user is present");
-//
-//            if (encoder.matches(password, user.get().getPassword())) {
-//                logger.info("passwords match");
-//                switch (user.get().getRole().toString()) {
-//                    case "ADMIN":
-//                        url = "adminHomepage";
-//                        logger.info("user: {}, role is {}", user.get().getUsername(), user.get().getRole().toString());
-//                        return adminHomepage(model, user.get());
-//
-//                    case "MANAGER":
-//                        url = "managerHomepage";
-//                        logger.info("user: {}, role is {}", user.get().getUsername(), user.get().getRole().toString());
-//                        return managerHomepage(model, user.get());
-//
-//
-//                    case "EMPLOYEE":
-//                        url = "employee_homepage";
-//                        logger.info("user: {}, role is {}", user.get().getUsername(), user.get().getRole().toString());
-//                        return employeeHomepage(model, user.get());
-//
-//                }
-//            } else {
-//                logger.info("passwords do not match");
-//            }
-//
-//        } else {
-//            logger.info("user is not present");
-//        }
-//
-//        return url;
-//
-//    }
-
 
 }
